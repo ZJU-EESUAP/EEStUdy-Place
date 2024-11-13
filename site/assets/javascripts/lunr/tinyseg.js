92dca5ca7f5ba888dc1bc76f0c01dd62ca8fbd70
Firisy Feng <irisvigor@icloud.com>
2024-11-13 17:11:55 +0800

    +学习手册内容

--- site/assets/javascripts/lunr/tinyseg.js
+++ site/assets/javascripts/lunr/tinyseg.js
@@ -3,10 +3,10 @@
  * Export code from https://github.com/umdjs/umd/blob/master/returnExports.js
  */
 ;(function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
+    if (typeof define ===  'function' && define.amd) {
         // AMD. Register as an anonymous module.
         define(factory)
-    } else if (typeof exports === 'object') {
+    } else if (typeof exports ===  'object') {
         /**
          * Node. Does not work with strict CommonJS, but
          * only CommonJS-like environments that support module.exports,
